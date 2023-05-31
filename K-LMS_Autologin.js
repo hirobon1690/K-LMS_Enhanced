039cac2c8e135d7b8aa423b7be53d28b82da7ab2
hirobon1690 <hirobon1690@gmail.com>
2023-06-01 04:12:30 +0900

    仕様変更に合わせてアップデート

--- K-LMS_Autologin.js
+++ K-LMS_Autologin.js
@@ -4,14 +4,14 @@
 // @version      0.1
 // @description  K-LMS AutoLogin
 // @author       You
-// @match        https://lms.keio.jp/courses/26335
+// @match        https://lms.keio.jp/portal.html
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=keio.jp
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    if(String(location)=="https://lms.keio.jp/courses/26335"){
+    if(String(location)=="https://lms.keio.jp/portal.html"){
         window.location.href = "https://lms.keio.jp/login/saml";
     }
 })();
